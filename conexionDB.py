804c0d00f757d81be754dfd5a653eb9f10d39a9c
Victor Adrian Garcia Estevez <victor.garciae@atos.net>
2022-03-14 14:15:53 +0100

    master

--- conexionDB.py
+++ conexionDB.py
@@ -11,7 +11,7 @@ select = conn.cursor()
 registros =  select.execute('select * from lbj.cam_client_type')
 
     
-print("CLITP_OID - CLITP_DESCRIPTION")
+print("CLITP_OID - CLITP_DESCRIPTIONs")
 print("-----------------------------")
 for row in registros:
     print(row[0] ," - ", row[1])
